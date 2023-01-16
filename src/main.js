17d429be6e298723cc585e6b49b590ab870d08cf
FlamesX-128 <flamesx128@gmail.com>
2023-01-16 16:14:29 -0600

    store

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import store from "./store";
 
 const app = createApp(App);
 
-app.use(router);
-
-app.mount("#app");
+app.use(router).use(store).mount("#app");
