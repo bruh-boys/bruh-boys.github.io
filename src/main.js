6430585ee84658a84d3153eb13ca5afa228da8e3
leonard S <MatheoJE_2000@hotmail.com>
2023-02-16 22:49:58 -0500

    new

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
 import store from "./store";
 
 const app = createApp(App);
-
 app.use(router).use(store).mount("#app");
+
